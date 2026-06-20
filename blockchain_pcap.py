df9ff9f2d897ee17e67b27c7342c890a13cb3d1a
gilangnurmawan628-ship-it <gilangnurmawan628@gmail.com>
2026-06-20 13:36:07 +0700

    Create blockchain_pcap.py

---
+++ blockchain_pcap.py
@@ -0,0 +1,65 @@
+import hashlib
+import json
+from datetime import datetime
+
+class Block:
+    def __init__(self, index, timestamp, evidence_file,
+                 packet_count, evidence_hash, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.evidence_file = evidence_file
+        self.packet_count = packet_count
+        self.evidence_hash = evidence_hash
+        self.previous_hash = previous_hash
+        self.block_hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_data = {
+            "index": self.index,
+            "timestamp": self.timestamp,
+            "evidence_file": self.evidence_file,
+            "packet_count": self.packet_count,
+            "evidence_hash": self.evidence_hash,
+            "previous_hash": self.previous_hash
+        }
+
+        return hashlib.sha256(
+            json.dumps(block_data, sort_keys=True).encode()
+        ).hexdigest()
+
+# Genesis Block
+blockchain = []
+
+genesis = Block(
+    0,
+    str(datetime.now()),
+    "Genesis",
+    0,
+    "0",
+    "0"
+)
+
+blockchain.append(genesis)
+
+# Contoh Evidence Block
+for i in range(1, 6):
+    previous_hash = blockchain[-1].block_hash
+
+    block = Block(
+        i,
+        str(datetime.now()),
+        f"PCAP0{i}.pcap",
+        i * 20,
+        hashlib.sha256(f"PCAP0{i}".encode()).hexdigest(),
+        previous_hash
+    )
+
+    blockchain.append(block)
+
+# Tampilkan Blockchain
+for block in blockchain:
+    print("Index:", block.index)
+    print("File:", block.evidence_file)
+    print("Previous Hash:", block.previous_hash)
+    print("Block Hash:", block.block_hash)
+    print("-" * 50)
